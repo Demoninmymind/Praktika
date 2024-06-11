6489c7329f53507cf5804facac8b38fac549e677
DEmoninmymind <nikita2004611@gmail.com>
2024-06-12 00:52:42 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+DZ = 12
+SrVr = 1.5
+Python = "Python"
+SrVr_DZ = DZ / SrVr
+print('Курс -', Python, "; Количество ДЗ =", DZ, '; Затрачиваемое время =', SrVr, '; Среднее время выполнения дз =', SrVr_DZ)
